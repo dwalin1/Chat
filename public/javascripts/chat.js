6368566d366f66c4b26aa95fcf0ef1307b3587b7
App Academy Student <student@appacademy.io>
2014-07-22 18:05:21 -0700

    End of day - rooms work, except that the users list does not yet update properly

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -10,6 +10,8 @@
     if (message.slice(0,5) =='/nick') {
       this.socket.emit('nicknameChangeRequest', message.slice(5, message.length - 1));
       console.log(message.slice(5, message.length - 1));
+    } else if (message.slice(0,5) =="/room") {
+      this.socket.emit('roomChangeRequest', message.slice(5, message.length - 1));
     } else {
       this.socket.emit('from_browser_event', { message: message });
     }
